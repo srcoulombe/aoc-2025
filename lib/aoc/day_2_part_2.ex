ea4edea48e5dd60f697ed0f9241807c525972533
Samy Coulombe <samy.coulombe@mail.mcgill.ca>
2025-12-03 19:25:47 -0500

    solution for puzzle 2 part 2

---
+++ lib/aoc/day_2_part_2.ex
@@ -0,0 +1,56 @@
+defmodule AOC.Day2Part2 do
+  alias AOC.FileParsing
+
+  def run(path) do
+    path
+    |> get_puzzle_input()
+    |> Enum.filter(fn {start, stop} -> start <= stop end)
+    |> Enum.flat_map(&find_invalid_numbers/1)
+    |> Enum.reduce(0, fn invalid_number, acc -> acc + invalid_number end)
+    |> IO.puts()
+  end
+
+  def get_puzzle_input(path) do
+    path
+    |> FileParsing.read_file_content()
+    |> String.split(",")
+    |> Enum.map(fn range ->
+      [start, stop] = String.split(range, "-")
+      {String.to_integer(start), String.to_integer(stop)}
+    end)
+  end
+
+  def find_invalid_numbers({start, stop}) when start >= stop, do: 0
+
+  def find_invalid_numbers({start, stop}) do
+    Range.new(start, stop)
+    |> Enum.to_list()
+    |> Enum.filter(&is_invalid?/1)
+  end
+
+  def is_invalid?(number) when number < 10, do: false
+
+  def is_invalid?(number) do
+    number_as_string = Integer.to_string(number)
+    num_digits = String.length(number_as_string)
+
+    Enum.to_list(1..div(num_digits,2))
+    |> Enum.any?(fn size ->
+      pattern =
+        number_as_string
+        |> String.slice(0, size)
+
+      rest =
+        number_as_string
+        |> String.slice(size, num_digits - size)
+
+      num_matches =
+        Regex.compile(pattern)
+        |> elem(1)
+        |> Regex.scan(rest)
+        |> Enum.count()
+
+      num_matches == ((num_digits - size) / size)
+    end)
+  end
+end
